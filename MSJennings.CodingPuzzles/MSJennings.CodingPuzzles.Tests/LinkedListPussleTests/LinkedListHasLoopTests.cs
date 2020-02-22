19afdf6d7dc07e02e36c6d497b53e51fd111c38c
Michael Jennings <michaelsjennings@outlook.com>
2020-02-22 10:58:03 -0500

    Add linked list has loop puzzle to solve

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Tests/LinkedListPussleTests/LinkedListHasLoopTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSJennings.CodingPuzzles.Work.LinkesListPuzzles;
+
+namespace MSJennings.CodingPuzzles.Tests.LinkedListPussleTests
+{
+    [TestClass]
+    public class LinkedListHasLoopTests
+    {
+        [TestMethod]
+        public void GetResult_WithLoop_ShouldReturnTrue()
+        {
+            // Arrange
+            var list = new SinglyLinkedList<int>();
+
+            list.Root = new SinglyLinkedListNode<int> { Value = 101 };
+
+            var two = new SinglyLinkedListNode<int> { Value = 102 };
+            list.Root.Next = two;
+
+            var three = new SinglyLinkedListNode<int> { Value = 103 };
+            two.Next = three;
+
+            var four = new SinglyLinkedListNode<int> { Value = 104 };
+            three.Next = four;
+
+            four.Next = two;
+
+            var sut = new LinkedListHasLoop();
+
+            // Act
+            var result = sut.GetResult(list);
+
+            // Assert
+            Assert.IsTrue(result, "Result is not correct");
+        }
+
+        [TestMethod]
+        public void GetResult_WithNoLoop_ShouldReturnFalse()
+        {
+            // Arrange
+            var list = new SinglyLinkedList<int>();
+
+            list.Root = new SinglyLinkedListNode<int> { Value = 101 };
+
+            var two = new SinglyLinkedListNode<int> { Value = 102 };
+            list.Root.Next = two;
+
+            var three = new SinglyLinkedListNode<int> { Value = 103 };
+            two.Next = three;
+
+            var four = new SinglyLinkedListNode<int> { Value = 104 };
+            three.Next = four;
+
+            var sut = new LinkedListHasLoop();
+
+            // Act
+            var result = sut.GetResult(list);
+
+            // Assert
+            Assert.IsFalse(result, "Result is not correct");
+        }
+    }
+}
---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/LinkesListPuzzles/LinkedListHasLoop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSJennings.CodingPuzzles.Work.LinkesListPuzzles
+{
+    public class LinkedListHasLoop
+    {
+        public bool GetResult<T>(SinglyLinkedList<T> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
