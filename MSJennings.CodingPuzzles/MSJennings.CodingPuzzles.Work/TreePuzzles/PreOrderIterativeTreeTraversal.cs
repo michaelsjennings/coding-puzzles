674022017c225601a97aa04aefba813017156127
Michael Jennings <mijennin@microsoft.com>
2020-02-23 21:11:54 -0500

    Add iterative tree traversal puzzle and solution

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/TreePuzzles/PreOrderIterativeTreeTraversal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace MSJennings.CodingPuzzles.Work.TreePuzzles
+{
+    public class PreOrderIterativeTreeTraversal
+    {
+        #region Public Methods
+
+        public int[] GetResult(Tree tree)
+        {
+            var result = new List<int>();
+
+            var nodeStack = new Stack<TreeNode>();
+            nodeStack.Push(tree.Root);
+
+            while (nodeStack.Count > 0)
+            {
+                var node = nodeStack.Pop();
+
+                result.Add(node.Value);
+
+                if (node.Right != null)
+                {
+                    nodeStack.Push(node.Right);
+                }
+
+                if (node.Left != null)
+                {
+                    nodeStack.Push(node.Left);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        #endregion Public Methods
+    }
+}
