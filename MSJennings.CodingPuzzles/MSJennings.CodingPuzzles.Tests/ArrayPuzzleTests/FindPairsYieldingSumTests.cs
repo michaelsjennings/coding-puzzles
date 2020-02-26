85681af9aa6992410d4813ec04975bf760678464
Michael Jennings <mijennin@microsoft.com>
2020-02-25 22:04:10 -0500

    Add FindPairsYieldingSum to solve

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Tests/ArrayPuzzleTests/FindPairsYieldingSumTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSJennings.CodingPuzzles.Work.ArrayPuzzles;
+using System.Collections.Generic;
+
+namespace MSJennings.CodingPuzzles.Tests.ArrayPuzzleTests
+{
+    [TestClass]
+    public class FindPairsYieldingSumTests
+    {
+        #region Public Methods
+
+        [TestMethod]
+        public void GetResult_ShouldReturnCorrectResult()
+        {
+            // Arrange
+            var sut = new FindPairsYieldingSum();
+
+            var arr = new[] { 0, 14, -3, 4, 7, 8, 3, 5, 7 };
+            var sum = 11;
+
+            IList<(int, int)> expected = new List<(int, int)>
+            {
+                (14, -3),
+                (4, 7),
+                (4, 7),
+                (8, 3),
+            };
+
+            // Act
+            var result = sut.GetResult(arr, sum);
+
+            // Assert
+            Assert.AreEqual(expected.Count, result.Count, "Number of result items is not correct");
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], result[i], "Value of a result item is not correct");
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/ArrayPuzzles/FindPairsYieldingSum.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MSJennings.CodingPuzzles.Work.ArrayPuzzles
+{
+    public class FindPairsYieldingSum
+    {
+        #region Public Methods
+
+        public IList<(int, int)> GetResult(int[] arr, int sum)
+        {
+            var result = new List<(int, int)>();
+
+            // brute force - O(n^2)
+            /*
+            for (var i = 0; i < arr.Length; i++)
+            {
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == sum)
+                    {
+                        result.Add((arr[i], arr[j]));
+                    }
+                }
+            }
+            */
+
+            return result;
+        }
+
+        #endregion Public Methods
+    }
+}
