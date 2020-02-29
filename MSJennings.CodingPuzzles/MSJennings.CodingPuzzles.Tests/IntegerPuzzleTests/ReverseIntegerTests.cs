8ee3a614ab9bc15cce5082f331fdc3bf273b2ed7
Michael Jennings <michaelsjennings@outlook.com>
2020-02-29 10:08:31 -0500

    Reverse integer

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Tests/IntegerPuzzleTests/ReverseIntegerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSJennings.CodingPuzzles.Work.IntegerPuzzles;
+
+namespace MSJennings.CodingPuzzles.Tests.IntegerPuzzleTests
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        [DataTestMethod]
+        [DataRow(123, 321)]
+        [DataRow(-123, -321)]
+        [DataRow(120, 21)]
+        public void GetResult_ShouldReturnCorrectResult(int value, int expectedResult)
+        {
+            // Arrange
+            var sut = new ReverseInteger();
+
+            // Act
+            var result = sut.GetResult(value);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result, "Result is not correct");
+        }
+    }
+}
---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/IntegerPuzzles/ReverseInteger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MSJennings.CodingPuzzles.Work.IntegerPuzzles
+{
+    public class ReverseInteger
+    {
+        public int GetResult(int value)
+        {
+            var result = 0;
+
+            while (value != 0)
+            {
+                var modResult = value % 10;
+
+                result = result * 10 + modResult;
+                value = value / 10;
+            }
+
+            return result;
+        }
+    }
+}
