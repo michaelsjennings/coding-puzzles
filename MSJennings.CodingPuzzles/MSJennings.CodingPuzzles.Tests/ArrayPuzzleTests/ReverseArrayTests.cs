97ff8050e04edebe477153e54c81c8a733770690
Michael Jennings <mijennin@microsoft.com>
2020-03-01 19:51:03 -0500

    Reverse array

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Tests/ArrayPuzzleTests/ReverseArrayTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSJennings.CodingPuzzles.Work.ArrayPuzzles;
+
+namespace MSJennings.CodingPuzzles.Tests.ArrayPuzzleTests
+{
+    [TestClass]
+    public class ReverseArrayTests
+    {
+        #region Public Methods
+
+        [TestMethod]
+        public void Go_WithEvenCount_ShouldReverse()
+        {
+            // Arrange
+            var sut = new ReverseArray();
+
+            var arr = new[] { 1, 2, 3, 4, 5, 6 };
+            var expected = new[] { 6, 5, 4, 3, 2, 1 };
+
+            // Act
+            sut.Go(arr);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, arr, "Array was not correctly reversed");
+        }
+
+        [TestMethod]
+        public void Go_WithOddCount_ShouldReverse()
+        {
+            // Arrange
+            var sut = new ReverseArray();
+
+            var arr = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var expected = new[] { 7, 6, 5, 4, 3, 2, 1 };
+
+            // Act
+            sut.Go(arr);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, arr, "Array was not correctly reversed");
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/ArrayPuzzles/ReverseArray.cs
@@ -0,0 +1,25 @@
+﻿namespace MSJennings.CodingPuzzles.Work.ArrayPuzzles
+{
+    public class ReverseArray
+    {
+        #region Public Methods
+
+        public void Go(int[] arr)
+        {
+            var i = 0;
+            var j = arr.Length - 1;
+
+            while (i < j)
+            {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+                j--;
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
