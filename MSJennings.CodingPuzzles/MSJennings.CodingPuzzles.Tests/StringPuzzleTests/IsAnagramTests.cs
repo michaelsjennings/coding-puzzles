57fd7e46a0fb2667aabf3634ad44c839d143dc05
Michael Jennings <michaelsjennings@outlook.com>
2020-03-03 14:33:58 -0500

    Add IsAnagram

---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Tests/StringPuzzleTests/IsAnagramTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSJennings.CodingPuzzles.Work.StringPuzzles;
+
+namespace MSJennings.CodingPuzzles.Tests.StringPuzzleTests
+{
+    [TestClass]
+    public class IsAnagramTests
+    {
+        [DataTestMethod]
+        [DataRow("iceman", "cinema", true)]
+        [DataRow("dog", "cat", false)]
+        [DataRow("moon", "mon", false)]
+        public void GetResult_ShouldReturnCorrectResult(string s1, string s2, bool expectedResult)
+        {
+            // Arrange
+            var sut = new IsAnagram();
+
+            // Act
+            var result = sut.GetResult(s1, s2);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result, "Result is incorrect");
+        }
+    }
+}
---
+++ MSJennings.CodingPuzzles/MSJennings.CodingPuzzles.Work/StringPuzzles/IsAnagram.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace MSJennings.CodingPuzzles.Work.StringPuzzles
+{
+    public class IsAnagram
+    {
+        public bool GetResult(string s1, string s2)
+        {
+            if (s1 is null || s2 is null)
+            {
+                return false;
+            }
+
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            IDictionary<char, int> GetCharacterCounts(string s)
+            {
+                var characterCounts = new Dictionary<char, int>();
+
+                for (var i = 0; i < s.Length; i++)
+                {
+                    var c = s[i];
+
+                    if (characterCounts.TryGetValue(c, out var count))
+                    {
+                        characterCounts[c] = count + 1;
+                    }
+                    else
+                    {
+                        characterCounts[c] = 1;
+                    }
+                }
+
+                return characterCounts;
+            }
+
+            var dic1 = GetCharacterCounts(s1);
+            var dic2 = GetCharacterCounts(s2);
+
+            foreach (var item in dic1)
+            {
+                if (dic2.TryGetValue(item.Key, out var count))
+                {
+                    if (count != item.Value)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
